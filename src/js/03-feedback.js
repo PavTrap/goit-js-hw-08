75872744addbd1609cd9c105166faa9982acfb2f
Pavel_soundNotLight <105560877+PavTrap@users.noreply.github.com>
2023-03-14 00:19:20 +0200

    console.log array form value

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,50 +1,40 @@
+////////////////////////////// IMPORTS //////////////////////////////
 import throttle from 'lodash.throttle';
 
-// найти эл...
-const formElement = document.querySelector('.feedback-form');
-const email = formElement[0];
-const message = formElement[1];
-
+////////////////////////////// EXPORT //////////////////////////////
+const formRef = document.querySelector('.feedback-form');
+const emailRef = document.querySelector('input[name="email"]');
+const messageRef = document.querySelector('textarea[name="message"]');
+const dataBaseArr = {
+    email: '',
+    message: '',
+};
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
 
-// слушатель на кнопку и форму
+/////////////////////////////// EVENTS //////////////////////////////
+formRef.addEventListener('submit', submitForm);
 
-formElement.addEventListener('input', throttle(inputElementStorage, 500))
-formElement.addEventListener('submit', handleFormSubmit);
+formRef.addEventListener('input', throttle(validateForm, 500));
 
-reproductionForm();
-
-// функция которая принимает данние из формы и сохраняет в локальное хранилище
-function inputElementStorage(e) {
-  const currentData = { email: email.value, message: message.value };
-
-  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentData));
-
+////////////////////////////// FUNCTIONS //////////////////////////////
+function validateForm(e){
+    const { name, value } = e.target;
+    dataBaseArr[name] = value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(dataBaseArr));
 }
-
-
-
-
-function handleFormSubmit(e) {
-  e.preventDefault();
-  const form = e.currentTarget;
-
-  if (!form.email.value.trim() || !form.message.value.trim()) {
-    return alert('Please fill in all the fields in form!')
-  }
-
-   e.currentTarget.reset();
-  localStorage.removeItem(LOCALSTORAGE_KEY);
-
+function submitForm (e){
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    console.log(dataBaseArr);
 }
+function getDataBase(){
+const getData = localStorage.getItem(LOCALSTORAGE_KEY);
+const parsedData = JSON.parse(getData);
+if (parsedData) {
+    emailRef.value = parsedData.email;
+    messageRef.value = parsedData.message;
+}}
 
-function reproductionForm() {
-
-  const parseData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-
-  if (parseData) {
-    email.value = parseData.email || '';
-    message.value = parseData.message || '';
-  }
-}
+getDataBase();
